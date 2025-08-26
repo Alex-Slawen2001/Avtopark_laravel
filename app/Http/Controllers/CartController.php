bcebb87167e2195a26f489c5dde9c4e6a3e7fc60
Alex-Slawen2001 <a.shumikhin01@mail.ru>
2025-08-26 21:40:01 +0500

    Реализовал контроллер корзины

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Car;
+class CartController extends Controller
+{
+    public function cart()
+    {
+        $cart = session()->get('cart');
+        return view('cart', compact('cart'));
+    }
+    public function addToCart(Request $request, $id) {
+        $car = Car::find($id);
+        $cart = session()->get('cart', []);
+        if(isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "name" => $car->name,
+                "quantity" => 1,
+                "price" => $car->price,
+            ];
+        }
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Product added to cart successfully!');
+    }
+    public function updateCart(Request $request)
+    {
+        if($request->id && $request->quantity){
+            $cart = session()->get('cart');
+            $cart[$request->id]["quantity"] = $request->quantity;
+            session()->put('cart', $cart);
+            session()->flash('success', 'Cart updated successfully');
+        }
+    }
+    public function removeFromCart(Request $request)
+    {
+        if($request->id) {
+            $cart = session()->get('cart');
+            if(isset($cart[$request->id])) {
+                unset($cart[$request->id]);
+                session()->put('cart', $cart);
+            }
+            session()->flash('success', 'Product removed successfully');
+        }
+    }
+}
